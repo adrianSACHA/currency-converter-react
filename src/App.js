92388a196984a8b719b0df7158d8b9ec51a4ee6b
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-21 13:13:48 +0100

    paste my old index.html site to react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form class="form js-form">
+        <fieldset class="form__fieldset">
+            <legend class="form__legend">
+                Kalkulator Walutowy - aktualny na 12.10.2022
+            </legend>
+            <p>
+                <label>
+                    Wpisz kwotę jaką chcesz policzyć*:
+                    <input class="form__field js-ammount" type="number" name="ammount" step="0.01" required>
+                </label>
+            </p>
+            <p>
+                <label>
+                    Wybierz walutę:
+                    <select class="form__field js-currency" name="currency">
+                        <option value="GBP">Funt</option>
+                        <option value="USD">Dolar</option>
+                    </select>
+                </label>
+            </p>
+            <p>
+                <label>
+                    Otrzymasz: <strong class="js-result">N/A
+                    </strong>
+                </label>
+            </p>
+        </fieldset>
+        <button class="form__button">Przelicz!</button>
+    </form>
+    <p>
+        *pole obowiązkowe
+    </p>
     </div>
   );
 }
