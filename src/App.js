845adac92ac779a05432ee5353c6692c239507b7
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-22 13:24:36 +0100

    added form component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-import './index.css';
-import Header from "./Header"
-import Container from "./Container"
-// import Form from "./Form";
+import "./index.css";
+import Header from "./Header";
+import Container from "./Container";
+import Form from "./Form";
 import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
   return (
     <>
       <Container>
-            <Header 
-              title="Kalkulator Walutowy - aktualny na 12.10.2022"
-            />
-            <form className="form">
-                <label>
-                    Wpisz kwotę jaką chcesz policzyć*:
-                  <input className="form__field" type="number" name="ammount" step="0.01" required />
-                  </label>
-                  <label>
-                    Wybierz walutę:
-                  <select className="form__field" name="currency">
-                        <option value="GBP">Funt</option>
-                        <option value="USD">Dolar</option>
-                  </select>
-                </label>
-                <label>
-                    Otrzymasz: <strong>N/A
-                    </strong>
-                </label>
-      
-        <button className="form__button">Przelicz!</button>
-     </form>
-    <Footer
-    title="pole obowiązkowe"
-     />
-    </Container>
-  </>
-      );
-  }
+        <Header title="Kalkulator Walutowy - aktualny na 12.10.2022" />
+        <Form />
+        
+        <Footer title="* pole obowiązkowe" />
+      </Container>
+    </>
+  );
+}
 
 export default App;
