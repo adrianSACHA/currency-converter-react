862d92502e4e030f6ef7ce2c2c3f004a31fc1956
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-22 13:57:22 +0100

    added functions result, set result, form

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import "./index.css";
 import Header from "./Header";
 import Container from "./Container";
 import Form from "./Form";
+import Result from "./Result";
 import Footer from "./Footer";
 import { useState } from "react";
+import { currencies } from "./currencies/index";
 
 function App() {
+  const [result, setResult] = useState(null);
+
+  const calculateResult = (ammountExchange, currency) => {
+    const rateExchange = currencies.find(
+      ({ short }) => short === currency
+    ).rate;
+    const currencyFinal = currencies.find(
+      ({ short }) => short === currency
+    ).short;
+
+    setResult((ammountExchange / rateExchange).toFixed(2) + currencyFinal);
+  };
+
   return (
-    <>
-      <Container>
-        <Header title="Kalkulator Walutowy - aktualny na 12.10.2022" />
-        <Form />
-        
-        <Footer title="* pole obowiązkowe" />
-      </Container>
-    </>
+    <Container>
+      <Header title="Kalkulator Walutowy - aktualny na 12.10.2022" />
+      <Form
+        calculateResult={calculateResult}
+        body={<Result result={result} />}
+      />
+       
+      <Footer title="* pole obowiązkowe" />
+    </Container>
   );
 }
 
