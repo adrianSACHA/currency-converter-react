af19ba677732744f4090f1aae1b8e66a0d18ed55
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-22 13:23:43 +0100

    added currencies

---
+++ src/currencies/index.js
@@ -0,0 +1,12 @@
+export const currencies = [
+  {
+    short: "USD",
+    name: "💵Dolar Amerykański",
+    rate: 4.9382,
+  },
+  {
+    short: "GBP",
+    name: "💷Funt Szterling",
+    rate: 5.5168,
+  },
+];
