65af8c19179edb0da172e6c635bc0f9441cfa8e9
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-24 12:15:40 +0100

    moved to App folder

---
+++ src/App/Footer/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+const Footer = ({ title }) => 
+    <div className="footer">
+        {title}
+    </div>;
+
+export default Footer;
---
+++ src/App/Result/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const Result = ({ result }) => (
+  <div className="result">
+    Otrzymasz:{" "}
+    <strong>{result}</strong>
+  </div>
+);
+
+export default Result;
