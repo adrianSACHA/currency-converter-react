ed2980cb3dcc8137a87d7a40bce832ed059da2af
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2022-11-28 09:47:25 +0100

    used formatted date and template strings

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -14,21 +14,18 @@ const Clock = () => {
     };
   }, []);
 
-  
+  const formattedDate = date.toLocaleDateString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 
   return (
     <p className="clock">
-      <span>
-        {" "}
-        Dzisiaj jest{" "}
-        {date.toLocaleDateString("pl", {
-          weekday: "long",
-          day: "numeric",
-          month: "long",
-        })}
-      </span>
-      {","}&nbsp;
-      <span>{date.toLocaleTimeString()}</span>
+      <span>{`Dzisiaj jest ${formattedDate}`}</span>
     </p>
   );
 };
